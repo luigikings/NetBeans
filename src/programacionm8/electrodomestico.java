8bcd4f9aecea4d9b43c72e458e3d91743aeb19d3
luigi <luigi@DESKTOP-CKQEDFF>
2021-03-08 13:27:05 +0100

    electrodomestico

--- src/programacionm8/electrodomestico.java
+++ src/programacionm8/electrodomestico.java
@@ -9,6 +9,27 @@ package programacionm8;
  *
  * @author luigi
  */
-public class electrodomestico {
-    
+public class Electrodomestico extends Articulo{ 
+   private String gama;
+   private ClaseE clasificacion;
+   
+   public Electrodomestico(){}
+   
+   public Electrodomestico(String gama, String color, TallaSML talla, String codigo, String nombre, float precio, int stock){
+       super(codigo,nombre,precio,stock);
+       this.gama=gama;
+       this.clasificacion=clasificacion;
+   }
+   public String getGama(){
+   return gama;
+   }
+   public void setGama(){
+   this.gama=gama;
+   }
+   public ClaseE getClasificacion(){
+   return clasificacion;
+   }
+   public void setClasificacion(ClaseE clasificacion){
+   this.clasificacion=clasificacion;
+   }
 }
